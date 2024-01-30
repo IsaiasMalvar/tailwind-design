741c8b853bb892be26b5ab29aa8128914f1a84a5
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2024-01-30 14:49:40 +0000

    add card component and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Card from "./components/Card";
 import Nav from "./components/Nav";
 import ShoeDetail from "./components/ShoeDetail";
+import { SHOE_LIST } from "./constant";
 
 const App = () => {
   return (
     <div className=" p-10 xl:px-24 animate-fadeIn">
-      <Nav />
-      <ShoeDetail />
+      {/* <Nav />
+      <ShoeDetail /> */}
+      <Card item={SHOE_LIST[0]} />
     </div>
   );
 };
