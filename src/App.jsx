8df538ca5801c8fd3c48c00ebfa60176ac7cb4c7
Isaías Malvar Guíos <isaiasmg93@hotmail.com>
2024-01-30 19:26:54 +0000

    create grid layout for card items

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Card from "./components/Card";
 import Nav from "./components/Nav";
+import NewArrivalSection from "./components/NewArrivalSection";
 import ShoeDetail from "./components/ShoeDetail";
 import { SHOE_LIST } from "./constant";
 
 const App = () => {
   return (
     <div className=" p-10 xl:px-24 animate-fadeIn">
-      {/* <Nav />
-      <ShoeDetail /> */}
-      <Card item={SHOE_LIST[0]} />
+      <Nav />
+      <ShoeDetail />
+      <NewArrivalSection items={SHOE_LIST} />
     </div>
   );
 };
